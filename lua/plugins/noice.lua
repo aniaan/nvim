b62cf5c60295f067bbaec057700a64e06a9cee58
aniaan <hi@aniaan.dev>
2025-08-05 14:03:25 +0800

    classic bottom cmdline

--- lua/plugins/noice.lua
+++ lua/plugins/noice.lua
@@ -6,17 +6,12 @@ return {
       require("consts").NUI,
     },
     opts = {
+      cmdline = {
+        view = "cmdline",
+      },
       presets = {
         bottom_search = true,
-        command_palette = true,
         long_message_to_split = true,
-      },
-      views = {
-        cmdline_popup = {
-          position = {
-            row = "40%",
-          },
-        },
       },
       lsp = {
         progress = {
