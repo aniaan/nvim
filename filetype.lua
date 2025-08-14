d16a62908eab3ce56bd52c0504e72e7f94090e3d
aniaan <hi@aniaan.dev>
2025-08-14 20:02:18 +0800

    move filetype to options.lua

--- filetype.lua
+++
@@ -1,9 +0,0 @@
-vim.filetype.add({
-  pattern = {
-    [".*/kitty/.+%.conf"] = "kitty",
-    [".*/ghostty/config"] = "ghostty",
-    ["%.env%.[%w_.-]+"] = "sh",
-  },
-})
-vim.treesitter.language.register("bash", "kitty")
-vim.treesitter.language.register("bash", "ghostty")
