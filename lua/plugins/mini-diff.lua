8d165b2b09d58aee3ddfb9722ef9b9f5df61a2d4
aniaan <hi@aniaan.dev>
2025-08-22 14:12:14 +0800

    delete mini.pairs&use default mini.diff config

--- lua/plugins/mini-diff.lua
+++ lua/plugins/mini-diff.lua
@@ -3,14 +3,14 @@ return {
     require("consts").MINI_DIFF,
     event = "VeryLazy",
     opts = {
-      view = {
-        style = "sign",
-        signs = {
-          add = "┃",
-          change = "┃",
-          delete = "-",
-        },
-      },
+      -- view = {
+      -- style = "sign",
+      -- signs = {
+      --   add = "┃",
+      --   change = "┃",
+      --   delete = "-",
+      -- },
+      -- },
     },
   },
 }
