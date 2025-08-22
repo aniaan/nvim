4aa78f52783905366d88fae9a66f4d6fc07a85db
aniaan <hi@aniaan.dev>
2025-08-22 14:43:48 +0800

    f

--- lua/consts.lua
+++ lua/consts.lua
@@ -6,7 +6,6 @@ M.BLINK_CMP = "saghen/blink.cmp"
 M.CONFORM = "stevearc/conform.nvim"
 
 M.FLASH = "folke/flash.nvim"
-M.NOICE = "folke/noice.nvim"
 M.SNACKS = "folke/snacks.nvim"
 M.WHICH_KEY = "folke/which-key.nvim"
 M.LAZYDEV = "folke/lazydev.nvim"
@@ -14,16 +13,10 @@ M.LAZYDEV = "folke/lazydev.nvim"
 M.TREESITTER = "nvim-treesitter/nvim-treesitter"
 M.TREESITTER_OBJECTS = "nvim-treesitter/nvim-treesitter-textobjects"
 
-M.NUI = "MunifTanjim/nui.nvim"
-
 M.MARKDOWN = "MeanderingProgrammer/render-markdown.nvim"
 
 M.MINI_AI = "echasnovski/mini.ai"
 M.MINI_DIFF = "echasnovski/mini.diff"
 M.MINI_ICONS = "echasnovski/mini.icons"
-M.MINI_PAIRS = "echasnovski/mini.pairs"
-
-M.AVANTE = "yetone/avante.nvim"
-M.PLENARY = "nvim-lua/plenary.nvim"
 
 return M
