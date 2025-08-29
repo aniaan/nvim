172f844b0c17898adac5c4a823968293fabc6174
aniaan <hi@aniaan.dev>
2025-08-29 10:52:06 +0800

    org const

--- lua/consts.lua
+++ lua/consts.lua
@@ -5,20 +5,23 @@ M.LAZY_NVIM_URL = "https://github.com/folke/lazy.nvim.git"
 M.BLINK_CMP = "saghen/blink.cmp"
 M.CONFORM = "stevearc/conform.nvim"
 
-M.FLASH = "folke/flash.nvim"
-M.SNACKS = "folke/snacks.nvim"
-M.WHICH_KEY = "folke/which-key.nvim"
-M.LAZYDEV = "folke/lazydev.nvim"
+local folke = "folke/"
+M.FLASH = folke .. "flash.nvim"
+M.SNACKS = folke .. "snacks.nvim"
+M.WHICH_KEY = folke .. "which-key.nvim"
+M.LAZYDEV = folke .. "lazydev.nvim"
 
-M.TREESITTER = "nvim-treesitter/nvim-treesitter"
-M.TREESITTER_OBJECTS = "nvim-treesitter/nvim-treesitter-textobjects"
+local treesitter  = "nvim-treesitter/"
+M.TREESITTER = treesitter .. "nvim-treesitter"
+M.TREESITTER_OBJECTS = treesitter .. "nvim-treesitter-textobjects"
 
 M.MARKDOWN = "MeanderingProgrammer/render-markdown.nvim"
 
-M.MINI_AI = "nvim-mini/mini.ai"
-M.MINI_DIFF = "nvim-mini/mini.diff"
-M.MINI_ICONS = "nvim-mini/mini.icons"
-M.MINI_PAIRS = "nvim-mini/mini.pairs"
+local mini = "nvim-mini/"
+M.MINI_AI = mini .. "mini.ai"
+M.MINI_DIFF = mini .. "mini.diff"
+M.MINI_ICONS = mini .. "mini.icons"
+M.MINI_PAIRS = mini .. "mini.pairs"
 
 
 return M
